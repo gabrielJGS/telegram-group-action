e37c702f9c5a7bfc0eff0a1cb2e2450cbdd6117e
gabrielJGS <gabrielftjose@gmail.com>
2023-11-20 18:37:16 -0300

    add pull request to message options

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,20 @@ async function run(): Promise<void> {
     const messageThread: number | null = +core.getInput("message_thread_id");
     const text: string = core.getInput("text");
     core.info(JSON.stringify(ctx));
-    const message = `${text}
+    let message = `${text}
     <b>on:</b> <a href='${ctx.payload.repository.html_url}'>${ctx.payload.repository.full_name}</a>
     <b>by:</b> <a href='${ctx.payload.sender.html_url}'>${ctx.payload.sender.login}</a>
-    <b>${ctx.eventName}:</b> ${ctx.ref}
-    <a href='${ctx.payload.head_commit.url}'>${ctx.payload.head_commit.message}</a>`;
+    <b>${ctx.eventName}:</b> ${ctx.ref}`;
+    if (ctx.payload.head_commit) {
+      message += `<a href='${ctx.payload.head_commit.url}'>${ctx.payload.head_commit.message}</a>`;
+    } else {
+      if (ctx.payload.pull_request) {
+        message += `<b>${ctx.payload.pull_request.head.label}</b><a href='${ctx.payload.pull_request._links.html}'>${ctx.payload.pull_request.title}</a>`;
+      } else {
+        core.error("Isn't a commit or pull request, dumping data:");
+        core.info(JSON.stringify(ctx));
+      }
+    }
 
     if (!botToken || botToken.trim() === "" || !chatId) {
       throw new Error(
