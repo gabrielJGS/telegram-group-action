c10185055aa4a10058ee3395dd33acd5b7be540f
gabrielJGS <gabrielftjose@gmail.com>
2023-11-20 16:55:20 -0300

    infer Context type and upgrade message

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import * as core from "@actions/core";
+import { Context } from './@types/Context'
 import { context } from '@actions/github';
 import { SendMessageParams, TelegramApi } from "./telegram";
 
 async function run(): Promise<void> {
+  const ct: unknown = context; // Assign context as unknown
+  const ctx: Context = ct as Context;
   try {
     const botToken: string = core.getInput("bot-token");
     const chatId: number = +core.getInput("chat-id");
     const messageThread: number | null = +core.getInput("message_thread_id");
     const text: string = core.getInput("text");
     const message = `${text}
-    <b>on:</b> ${context.repo.repo} <b>by:</b>${context.actor}
-    <b>${context.eventName}:</b> ${context.ref}
-    <a href='${context.payload.head_commit.url}'>${context.payload.head_commit.message}</a>`
+    <b>on:</b> <a href='${ctx.payload.repository.html_url}'>${ctx.payload.repository.full_name}
+    <b>by:</b><a href='${ctx.payload.sender.html_url}'>${ctx.payload.sender.login}</a>
+    <b>${ctx.eventName}:</b> ${ctx.ref}
+    <a href='${ctx.payload.head_commit.url}'>${ctx.payload.head_commit.message}</a>`
     if (!botToken || botToken.trim() === "" || !chatId) {
       throw new Error(
         "bot-token and chat-id are required in github project secrets",
